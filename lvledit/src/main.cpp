3cfe722d4c4a43ca863ce08fbea18dfc26ca6bd1
carsonritchey <ritcheycarson@gmail.com>
2022-03-28 14:28:50 -0500

    colored grid and movement

--- lvledit/src/main.cpp
+++ lvledit/src/main.cpp
@@ -12,7 +12,7 @@
 #define GAME_VIEW_HEIGHT 600
 
 #define TILE_SIZE 16
-#define ZOOM_FACTOR 3
+#define ZOOM_FACTOR 2
 
 #define SEPERATOR ,
 #define EMPTY -1
@@ -27,23 +27,31 @@ int map_h = -1;
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "lvledit");
+    window.setFramerateLimit(30);
     sf::View view(sf::Vector2f(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), sf::Vector2f(WINDOW_WIDTH / ZOOM_FACTOR, WINDOW_HEIGHT / ZOOM_FACTOR));
 
 	int** map = loadMap(std::string("overworld.dat"));
 
     sf::Texture texture;
-    //texture.loadFromFile("../art/sprites/miku.png");
     texture.loadFromFile("/archive/pictures/random_pics/miku on github.png");
     sf::Sprite sprite;
     sprite.setTexture(texture);
 
 	while(window.isOpen()) {
 		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
+		while(window.pollEvent(event)) {
+			if(event.type == sf::Event::Closed)
 				window.close();
-		}	
+		}
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            view.move(-TILE_SIZE, 0);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            view.move(TILE_SIZE, 0);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            view.move(0, -TILE_SIZE);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            view.move(0, TILE_SIZE);
 
         window.setView(view);
 		window.clear();
@@ -68,18 +76,20 @@ void closeMap(int** map) {
 }
 
 void drawGrid(sf::RenderWindow* window) {
-    for(int i = 0; i < WINDOW_WIDTH; i += TILE_SIZE) {
+    sf::Color color(255, 255, 255, 100);
+
+    for(float i = 0; i < WINDOW_WIDTH; i += TILE_SIZE) {
         sf::Vertex line[] = {
-            sf::Vertex(sf::Vector2f(i, 0)),
-            sf::Vertex(sf::Vector2f(i, WINDOW_HEIGHT))
+            {{i, 0}, color},
+            {{i, WINDOW_HEIGHT}, color}
         };
 
         window->draw(line, 2, sf::Lines);
     }
-    for(int j = 0; j < WINDOW_HEIGHT; j += TILE_SIZE) {
+    for(float j = 0; j < WINDOW_HEIGHT; j += TILE_SIZE) {
         sf::Vertex line[] = {
-            sf::Vertex(sf::Vector2f(0, j)),
-            sf::Vertex(sf::Vector2f(WINDOW_WIDTH, j))
+            {{0, j}, color},
+            {{WINDOW_WIDTH, j}, color}
         };
 
         window->draw(line, 2, sf::Lines);
