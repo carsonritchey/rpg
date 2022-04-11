e370a3abff6c86150399eb67b02e898807652075
Carson Ritchey <carson@macbook.local>
2022-04-11 07:50:49 -0500

    collision detection fixed

--- src/player.cpp
+++ src/player.cpp
@@ -42,47 +42,49 @@ void Player::checkInput(const sf::Event* event) {
 }
 
 sf::Vector2f Player::movePlayer(const float dt, Map* map) {
+    sf::Vector2f pos; 
     sf::Vector2f dPos(0.f, 0.f);
+    int tile_x, tile_y;
 	if(up) {
         dPos.y -= mvnt_speed * dt;
+        pos = sprite.getPosition();
+        tile_x = (int)((pos.x + dPos.x) / TILE_SIZE / ZOOM_FACTOR);
+        tile_y = (int)((pos.y + dPos.y) / TILE_SIZE / ZOOM_FACTOR);
+        if(up && (map->tile_collision[map->map_h * tile_y + tile_x] == COLLISION_WALL || map->tile_collision[map->map_h * tile_y + tile_x + 1] == COLLISION_WALL)) {
+            dPos.y = 0;
+        }
     }
 	if(down) {
         dPos.y += mvnt_speed * dt;
+        pos = sprite.getPosition();
+        tile_x = (int)((pos.x + dPos.x) / TILE_SIZE / ZOOM_FACTOR);
+        tile_y = (int)((pos.y + dPos.y) / TILE_SIZE / ZOOM_FACTOR);
+        if(down && (map->tile_collision[map->map_h * (tile_y + 1) + tile_x] == COLLISION_WALL || map->tile_collision[map->map_h * (tile_y + 1) + tile_x + 1] == COLLISION_WALL)) {
+            dPos.y = 0;
+        }
     }
 	if(left) {
         dPos.x -= mvnt_speed * dt;
+        pos = sprite.getPosition();
+        tile_x = (int)((pos.x + dPos.x) / TILE_SIZE / ZOOM_FACTOR);
+        tile_y = (int)((pos.y + dPos.y) / TILE_SIZE / ZOOM_FACTOR);
+        if(left && (map->tile_collision[map->map_h * tile_y + tile_x] == COLLISION_WALL || map->tile_collision[map->map_h * (tile_y + 1) + tile_x] == COLLISION_WALL)) {
+            dPos.x = 0;
+        }
         if(!right)
             cycleTexture(l_frames, sizeof(l_frames) / sizeof(l_frames[0]));
     }
 	if(right) {
         dPos.x += mvnt_speed * dt;
+        pos = sprite.getPosition();
+        tile_x = (int)((pos.x + dPos.x) / TILE_SIZE / ZOOM_FACTOR);
+        tile_y = (int)((pos.y + dPos.y) / TILE_SIZE / ZOOM_FACTOR);
+        if(right && (map->tile_collision[map->map_h * tile_y + tile_x + 1] == COLLISION_WALL || map->tile_collision[map->map_h * (tile_y + 1) + tile_x + 1] == COLLISION_WALL)) {
+            dPos.x = 0;
+        }
         if(!left)
             cycleTexture(r_frames, sizeof(r_frames) / sizeof(r_frames[0]));
     }
-
-
-    sf::RectangleShape tile(sf::Vector2f(TILE_SIZE * ZOOM_FACTOR, TILE_SIZE * ZOOM_FACTOR));
-    sf::Vector2f pos = sprite.getPosition();
-    int tile_x = (int)(pos.x / TILE_SIZE / ZOOM_FACTOR);
-    int tile_y = (int)(pos.y / TILE_SIZE / ZOOM_FACTOR);
-
-    //std::cout << (int)pos.x / TILE_SIZE / ZOOM_FACTOR << " " << (int)pos.y / TILE_SIZE / ZOOM_FACTOR << std::endl;
-    //std::cout << map->tile_collision[map->map_h * (int)(pos.y / TILE_SIZE / ZOOM_FACTOR) + (int)(pos.x / TILE_SIZE / ZOOM_FACTOR)] << std::endl;
-    
-    ///*
-    if(up && (map->tile_collision[map->map_h * tile_y + tile_x] == COLLISION_WALL || map->tile_collision[map->map_h * tile_y + tile_x + 1] == COLLISION_WALL)) {
-        dPos.y = 0;
-    }
-    if(down && (map->tile_collision[map->map_h * (tile_y + 1) + tile_x] == COLLISION_WALL || map->tile_collision[map->map_h * (tile_y + 1) + tile_x + 1] == COLLISION_WALL)) {
-        dPos.y = 0;
-    }
-    if(left && (map->tile_collision[map->map_h * tile_y + tile_x] == COLLISION_WALL || map->tile_collision[map->map_h * (tile_y + 1) + tile_x] == COLLISION_WALL)) {
-        dPos.x = 0;
-    }
-    if(right && (map->tile_collision[map->map_h * tile_y + tile_x + 1] == COLLISION_WALL || map->tile_collision[map->map_h * (tile_y + 1) + tile_x + 1] == COLLISION_WALL)) {
-        dPos.x = 0;
-    }
-    //*/
 
     // TL edge of word
     if(pos.x + dPos.x < 0)
